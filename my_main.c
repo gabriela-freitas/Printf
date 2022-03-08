c6606dfe34a33dfc9b2a1ee692726f29a836e200
Gabriela Freitas de albuquerque <gafreita@c1r5s1.42lisboa.com>
2022-03-08 15:42:23 +0000

    submit

--- my_main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-
-int main ()
-{
-	int retorno = ft_printf("%x\n", 2147483647);
-	int retornop = printf("%x\n", 2147483647);
-	ft_printf("retorno: %d, retorno ok: %d", retorno, retornop);
-}
